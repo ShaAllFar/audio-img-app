fd7993fa77499ce448812bd788285c3cff0f704b
Shawn Farrow <Shaallfar@gmail.com>
2016-04-28 08:48:44 -0700

    initial push set up scaffolding stating laying out the code

---
+++ app.js
@@ -0,0 +1,26 @@
+var imgSongArray = [];
+
+function ImgSong(picName,picFilepath,audioName, audioFilePath){
+  this.picName = picName;
+  this.picFilepath = picFilepath;
+  this.audioName = audioName;
+  this.audioFilePath = audioFilePath;
+  imgSongArray.push(this);
+}
+new ImgSong('me','images/me.jpg','blessings','audio/blessings.mp3');
+
+function displayImgSong(){
+  var img = document.getElementById('img');
+  var player = document.getElementById('player');
+  img.src = imgSongArray[0].picFilepath;
+  player.src = imgSongArray[0].audioFilePath;
+}
+
+function playSong(){
+  var player = document.getElementById('player');
+  if(player.play()){
+    player.pause();
+  }else{
+    player.play();
+  }
+}
